63ecfbe60de500d499b49496b77fb84b088ae8f4
katzi-tantush <65847591+katzi-tantush@users.noreply.github.com>
2023-09-04 22:35:36 +0300

    remove cursor. enlarge text triggers

---
+++ Assets/Scriipts/StateManager/MouseBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+}
